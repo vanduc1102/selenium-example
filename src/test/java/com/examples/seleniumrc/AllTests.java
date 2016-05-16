8d604748afcf6b49fb89aeaf98370f77611790f3
DUC NGUYEN <vanducld@gmail.com>
2016-05-16 13:10:44 +0700

    Add firstSelectBox text case

--- src/test/java/com/examples/seleniumrc/AllTests.java
+++ src/test/java/com/examples/seleniumrc/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AppTest.class, TestCase1.class, TestCase2.class })
+@SuiteClasses({ AppTest.class, TestCase1.class, TestCase2.class, KyotokimonoRental.class })
 public class AllTests {
 
 }
