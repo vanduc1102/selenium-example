81a916876a4978def9b2b5eabaf338d1e7b3c3a6
DUC NGUYEN <vanducld@gmail.com>
2016-05-23 15:05:16 +0700

    Add new testCase calendarSelectionTest

--- src/test/java/com/examples/seleniumrc/KyotokimonoRentalTest.java
+++ src/test/java/com/examples/seleniumrc/KyotokimonoRentalTest.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class KyotokimonoRentalTest {
 	WebDriver driver;
@@ -47,19 +49,50 @@ public class KyotokimonoRentalTest {
 
 	@Test
 	public void getAlertMessage() throws InterruptedException {
-		WebElement checkBoxElement = driver.findElement(By.xpath(".//*[@id='formAddPlan']/div[1]/div/ul/li[1]/div[3]/div[2]/div[1]/a"));
+		WebElement checkBoxElement = driver
+				.findElement(By.xpath(".//*[@id='formAddPlan']/div[1]/div/ul/li[1]/div[3]/div[2]/div[1]/a"));
 		checkBoxElement.click();
-		Thread.sleep(500);		
-		
+		Thread.sleep(500);
+
 		Alert alert = driver.switchTo().alert();
-		
+
 		// Get the text of the alert or prompt
 		String message = alert.getText();
 		System.out.println("The message is : " + message);
-		
+
 		// And acknowledge the alert (equivalent to clicking "OK")
 		alert.accept();
 		Assert.assertEquals("男性か女性、いずれか1名以上選択してください", message);
-		
+
+	}
+
+	@Test
+	public void calendarSelectionTest() throws InterruptedException {
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		// Open first selectBox
+		WebElement checkBoxElement = driver.findElement(By.id("list_plans_1SelectBoxItContainer"));
+		checkBoxElement.click();
+
+		// select 3 people
+		WebElement itemElement3 = driver
+				.findElement(By.xpath(".//*[@id='list_plans_1SelectBoxItOptions']/li[4]/a/span[2]"));
+		itemElement3.click();
+
+		WebElement nextButton = driver
+				.findElement(By.xpath(".//*[@id='formAddPlan']/div[1]/div/ul/li[1]/div[3]/div[2]/div[1]/a"));
+		nextButton.click();
+
+		// Select second shop
+		By selectedShopByCss = By.cssSelector("#choose-shop li.shop-6 label");
+		wait.until(ExpectedConditions.elementToBeClickable(selectedShopByCss));
+		WebElement secondShop = driver.findElement(selectedShopByCss);
+		secondShop.click();
+
+		// Select a date.
+		WebElement dateElement = wait.until(ExpectedConditions
+				.elementToBeClickable(By.xpath(".//*[@id='choose-date']/div[2]/div/table/tbody/tr[2]/td[4]/div/div")));
+		dateElement.click();
+		System.out.println("Clicked element " + dateElement.getText());
+
 	}
 }
